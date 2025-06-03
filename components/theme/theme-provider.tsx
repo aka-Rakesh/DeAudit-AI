2b4bf1e27c66cf4624aac224b73d4a7cdfc7b071
Rakesh Sharma <120479281+aka-Rakesh@users.noreply.github.com>
2025-06-04 00:57:55 +0530

    fix v0.55

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import type { PropsWithChildren } from "react";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: PropsWithChildren<any>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
